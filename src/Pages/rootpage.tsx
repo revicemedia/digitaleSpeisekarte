462a4d6fdacb50d3a83ab819e99be06cd7b99c69
Fabian Hanso <f.hanso@revice-media.com>
2024-01-03 18:50:58 +0100

    delete size restriction

--- src/Pages/rootpage.tsx
+++ src/Pages/rootpage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Categories from "../Components/Categories/Categories";
 import Header from "../Components/Header/Header";
 import HeroSection from "../Components/HeroSection/HeroSection";
@@ -8,34 +8,19 @@ import Footer from "../Components/Footer/Footer";
 import News from "../Components/News/News";
 
 const RootPage: React.FC = () => {
-  const [isDesktop, setIsDesktop] = useState(false);
   const baseClass = "RootPage";
 
   // const { loading, error }: any = useContentful();
 
-  const log = window.innerWidth;
-
-  useEffect(() => {
-    if (log >= 700) {
-      setIsDesktop(true);
-    }
-  }, [log]);
-
   return (
     <>
-      {isDesktop ? (
-        <section>
-          Bitte wähle ein mobiles Endgerät oder nutze deinen Portrait-Modus!
-        </section>
-      ) : (
-        <section className={baseClass}>
-          <Header />
-          <HeroSection />
-          {config.options.locationHasNewsSlider && <News />}
-          <Categories />
-          <Footer />
-        </section>
-      )}
+      <section className={baseClass}>
+        <Header />
+        <HeroSection />
+        {config.options.locationHasNewsSlider && <News />}
+        <Categories />
+        <Footer />
+      </section>
     </>
   );
 };
