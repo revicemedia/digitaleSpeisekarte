109a1aec774235a4868208bbdf4980bf5c3db4ce
Fabian Hanso <f.hanso@revice-media.com>
2024-01-10 00:11:05 +0100

    Add city of location

--- src/Configs/config.ts
+++ src/Configs/config.ts
@@ -3,7 +3,7 @@ import { IConfig } from "../interfaces/ConfigInterface";
 export const config: IConfig = {
   locationName: "Du & Ich Limburg",
   locationStreet: "Musterstraße 23",
-  locationCity: "Musterstadt",
+  locationCity: "Limburg an der Lahn",
   locationPLZ: 56555,
   categoriesHeadline: "Kategorien",
   searchHeadline: "Suchen",
