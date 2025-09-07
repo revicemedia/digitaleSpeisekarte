e7d17a7fbfdfbf66e0b04cf17fe39bdd093190c9
Fabian Hanso <fabian.hanso@accenture.com>
2025-09-07 18:54:18 +0200

    set maintenance status

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootPage from "./Pages/rootpage";
 import ImpressumPage from "./Pages/impressum";
 import AllergenePage from "./Pages/allergene";
+import "./index.scss";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <div className="maintenance-wrapper">
+        <h2 className="maintenance-headline">
+          Diese Seite ist vorübergehend offline!
+        </h2>
+        <p className="maintenance-text">
+          Bitte wenden Sie sich <b>schnellstmöglich</b> an Ihren
+          Ansprechpartner!
+        </p>
+      </div>
+
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/*" element={<RootPage />} />
           <Route path="/allergene" element={<AllergenePage />} />
           <Route path="/impressum" element={<ImpressumPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 };
