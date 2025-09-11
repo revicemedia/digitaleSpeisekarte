3637db2f0dd27371cc064bc8cc42ce53f6008669
Fabian Hanso <fabian.hanso@accenture.com>
2025-09-11 14:41:08 +0200

    promise for payment on sunday so website back online

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootPage from "./Pages/rootpage";
 import ImpressumPage from "./Pages/impressum";
 import AllergenePage from "./Pages/allergene";
-import "./index.scss";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div className="maintenance-wrapper">
-        <h2 className="maintenance-headline">
-          Diese Seite ist vorübergehend offline!
-        </h2>
-        <p className="maintenance-text">
-          Bitte wenden Sie sich <b>schnellstmöglich</b> an Ihren
-          Ansprechpartner!
-        </p>
-      </div>
-
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/*" element={<RootPage />} />
           <Route path="/allergene" element={<AllergenePage />} />
           <Route path="/impressum" element={<ImpressumPage />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   );
 };
