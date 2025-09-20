638556632202fe20c0f44550b670ba16746ecb33
Fabian Hanso <fabian.hanso@accenture.com>
2025-09-20 13:49:53 +0200

    block site because payment not received

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootPage from "./Pages/rootpage";
 import ImpressumPage from "./Pages/impressum";
 import AllergenePage from "./Pages/allergene";
+import block from "./images/block.svg";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/*" element={<RootPage />} />
           <Route path="/allergene" element={<AllergenePage />} />
           <Route path="/impressum" element={<ImpressumPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <div className="blocked">
+        <img src={block} alt="Acces blocked" className="blockimage" />
+        <h2>Diese Seite wurde gesperrt</h2>
+        <p className="blockedtext">
+          Bitte wenden Sie sich zur Entsperrung an Ihren Ansprechpartner!
+        </p>
+      </div>
     </div>
   );
 };
