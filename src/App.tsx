e516ce6b1bbf758d10e02172166dde2501a34114
Fabian Hanso <fabian.hanso@accenture.com>
2025-09-22 15:40:36 +0200

    payment received on sept 22nd 2025

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootPage from "./Pages/rootpage";
 import ImpressumPage from "./Pages/impressum";
 import AllergenePage from "./Pages/allergene";
-import block from "./images/block.svg";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/*" element={<RootPage />} />
           <Route path="/allergene" element={<AllergenePage />} />
           <Route path="/impressum" element={<ImpressumPage />} />
         </Routes>
-      </BrowserRouter> */}
-      <div className="blocked">
-        <img src={block} alt="Acces blocked" className="blockimage" />
-        <h2>Diese Seite wurde gesperrt</h2>
-        <p className="blockedtext">
-          Bitte wenden Sie sich zur Entsperrung an Ihren Ansprechpartner!
-        </p>
-      </div>
+      </BrowserRouter>
     </div>
   );
 };
